4a147c2a736204e9f0ab6e10671398491c9789c9
TeeRation <180157973+TeeRation@users.noreply.github.com>
2026-09-14 06:37:47 +0300

    Add query runner script

---
+++ queries/000_run_all.sql
@@ -0,0 +1,8 @@
+\pset pager off
+
+\ir 001_select_buildings.sql
+\ir 002_select_workers_by_specialty.sql
+\ir 003_schedule_by_date_range.sql
+\ir 004_calculate_brigade_duration.sql
+\ir 005_count_brigade_members.sql
+\ir 006_materials_for_building.sql
